9fc30da00a0bd879e8a9353e6805ff0c1d474c14
ajaykesarwani <ajaykrkesarwani@gmail.com>
2020-03-09 07:59:24 +0530

    Update read_store.py

--- read_store.py
+++ read_store.py
@@ -32,32 +32,19 @@ data = {}
 class read_store:
     fileName=""
     def downloadFile(self):
-        print("Trying to connect to Remote Serve")
-        # copying the file from one folder to other
+        print("Coping only the CSV file from one folder to other!!!")
         now = str(datetime.now())[:19]
         now = now.replace(":", "_")
         src_dir = "C:\\Users\\USER\\Desktop\\Assignment\\SrcFile\\"
-        #dst_dir = ""#"C:\Users\USER\Desktop\Assignment\DestFile\SampleFile"+"_" + str(now) + ".csv"
-        #dst_dir = "C:\Users\USER\Desktop\Assignment\DestFile"
         for filename in os.listdir(src_dir):
-            print(filename)
+            #print("Name of File ",filename)
             _filename_ext = filename.split(".")
             dst_dir = "C:\\Users\USER\\Desktop\\Assignment\DestFile"+ '\\'+str(_filename_ext[0]) + "_" + str(now) + ".csv"
-            print(dst_dir)
-           # shutil.copy(filename+, dest_dir)
             shutil.copy(src_dir+filename, dst_dir)
 
     def readFile(self):
         fileNames = glob('C:\Users\USER\Desktop\Assignment\DestFile\*.csv')
-        print("printing the fileName")
-        print(type(fileNames))
-        print(fileNames)
-        type(fileNames)
-        #dataframes = [pd.read_csv(f) for f in fileNames]
         return fileNames
-
-        #print("printing the contain of file")
-        #print(dataframes[0])
 
     def connect_DB_store(self , fileNames):
         #Establishing Connection: Default port number: 27017
@@ -74,48 +61,27 @@ class read_store:
         db_cm = mng_db[collection]
 
         #reading csv file using pandas
-        list = []
         for i in range(len(fileNames)):
-            print("fileName...")
-            print(fileNames[i])
+            print("Reading fileName...", fileNames[i])
             file_data = pd.read_csv(fileNames[i])
-            print("FileData of")
-            print(fileNames[i])
-            print("file_data")
+            print("File Contain is")
             print(file_data)
             count_row = file_data.shape[0]
-            print("count_row")
-            print(count_row)
-
             for j in range(count_row-2):
-                print("firstRow")
                 print(file_data["Object Name"].iloc[1])
                 abc = str(file_data["Object Name"].iloc[1])
-                list = abc.split(",")
-                local_cell_Id = list[1].split("=")
+                object = abc.split(",")
+                local_cell_Id = object[1].split("=")
                 cell_Id = local_cell_Id[1]
-                print(cell_Id)
+                print("printing cell_Id",cell_Id)
                 file_data_ = file_data.iloc[1]
                 file_data_json = json.loads(file_data_.to_json(orient='records'))
-
-                key = cell_Id
-                data[key] = file_data_json
-                print(data)
-                print("after completin")
-                print(dict(data))
-                #data_json = json.dumps(dict(data))
-                #print(data_json)
+                data[cell_Id] = file_data_json
+                print("storing data in mongodb",data)
                 #store the data into DB
                 db_cm.remove()
                 db_cm.insert(data)
-                # inserting the as a dictionary where key is name of file and value is file as a document: into the mongoDB
-
-
-
-#if __name__ == "__main__":
-#    #filepath =""
-#    fileName = "SampleFile.csv"
-#    read_CSVfile_and_store(fileName)
+                # inserting the as a dictionary where key is cell_d  and value is row : into the mongoDB
 
 
 temp = read_store()
