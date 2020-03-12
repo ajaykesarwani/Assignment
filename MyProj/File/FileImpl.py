0add4661c3393b5c6d9672ddb9e9ad5c84349802
ajaykesarwani <ajaykrkesarwani@gmail.com>
2020-03-12 11:00:03 +0530

    Update FileImpl.py

--- MyProj/File/FileImpl.py
+++ MyProj/File/FileImpl.py
@@ -8,9 +8,6 @@ import multiprocessing
 import threading
 from datetime import datetime
 from File.file import FileFileOperation as absFile
-#from RS.SFTPConnectRS import SFtpConnectionRS
-#from DB.MongoDB import MongoDB
-#from src.utils.helper import get_cpu_count
 
 
 class FileImpl(absFile):
@@ -19,20 +16,7 @@ class FileImpl(absFile):
     def __init__(self, remotePath, localPath):
         self.remotePath = remotePath
         self.localPath = localPath
-        #self.downloadQueue = downloadQueue
-        #self.serverConnection = SFtpConnectRS()
-        #self.dbConnection = MongoDB()
-    """                                                         
-    @property
-    def get_local_path(self):
-        return self._localPath
-
-    @localPath.setter
-    def set_local_path(self, localPath):
-        if not os.path.exists(localPath):
-            raise Exception("Error occurred: local path %s does not exist" % localPath)
-        self._localPath = localPath
-"""
+        
     def download(self):
         #Here only coping from one folder to other folder
         now = str(datetime.now())[:19]
