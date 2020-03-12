cf139745a3a16af48032ecb6d00773d2363c94e2
ajaykesarwani <ajaykrkesarwani@gmail.com>
2020-03-12 11:01:31 +0530

    Update MongoDB.py

--- MyProj/DB/MongoDB.py
+++ MyProj/DB/MongoDB.py
@@ -11,15 +11,8 @@ class MongoDB(Database):
         self.db = conn.database  # replace with actual db
         self.collection = db.collection
 
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        if exc_val:
-            print("Error Occurred in MongoDB operation: %s" % exec_val)
-        self.connection.close()
-
+    
     def update(self, document):
         """
         """
-        self.db.update({cell_id: document["cell_id"]}, {$set: document}, {upsert: true})
+        self.db.update(document)
