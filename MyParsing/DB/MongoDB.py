9c2e216799bef5525ebc09361be6fdff0ad794ff
ajaykesarwani <ajaykrkesarwani@gmail.com>
2020-03-23 19:43:52 +0530

    Update MongoDB.py

--- MyParsing/DB/MongoDB.py
+++ MyParsing/DB/MongoDB.py
@@ -13,19 +13,20 @@ class MongoDB(Database):
     def __init__(self, uri, dataBase):
         #self.connection = MongoClient()
         self.connection = pymongo.MongoClient('localhost',27017)
-        self.db = dataBase # replace with actual db
+        self.db =  self.connection[dataBase]# replace with actual db
         self.collection = 'mycollection'
 
     def connect_mongodb(self, *params):
         pass
 
+    def get(self):
+        """
+        :return: data
+        """
+        return self.db['mycollection'].find()
+
+
     def insert(self, data):
         """
         """
-        print type(data)
-        document = json.loads(data)
-        print "the type of document"
-        print(type(document))
-        #print document
-        self.db.insert(document)
-        print "insertion done successfully"
+        self.db['mycollection'].insert(data)
